1dc961da49dad59c98aa13a06b3d5ee9289ff65d
wungthing <wungthingkashungnao123@gmail.com>
2023-09-15 20:41:53 +0530

    added design for the front card

--- src/components/frontcard/FrontCreditCard.jsx
+++ src/components/frontcard/FrontCreditCard.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import styles from "./frontCard.module.css";
 import frontCardImg from "../../images/frontCard.png";
+import blueE from "../../images/blue.png";
+import darkblue from "../../images/darkblue.png";
+import orange from "../../images/orange.png";
+import pink from "../../images/pink.png";
+import purple from "../../images/purple.png";
 
 const frontCard = ({ cardNum, name, month, year }) => {
   return (
     <div className={styles.container}>
       <div className={styles.innerCon}>
-        <img src={frontCardImg} alt="" />
+        <img src={blueE} alt="" className={styles.blueE} />
+        <img src={darkblue} alt="" className={styles.darkblue} />
+        <img src={orange} alt="" className={styles.orange} />
+        <img src={pink} alt="" className={styles.pink} />
+        <img src={purple} alt="" className={styles.purple} />
         <div className={styles.cardDetails}>
           <div className={styles.cardNumber}>
             {cardNum ? (
